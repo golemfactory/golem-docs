65827dfcb0c902067fe50edb5e03572f901219e3
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2023-08-08 12:55:23 +0200

    Fix hydration error

--- src/components/SelectionCard.jsx
+++ src/components/SelectionCard.jsx
@@ -19,7 +19,7 @@ export function SelectionCard({ children, icon, title, buttonText, href }) {
           {children.map((child, index) => (
             <div key={index} className="not-prose flex items-start gap-x-2">
               <CheckIcon className="mt-1.5 h-4 w-4 flex-shrink-0 fill-dark" />
-              <p className="text-dark">{child}</p>
+              <div className="text-base text-dark">{child}</div>
             </div>
           ))}
         </div>
