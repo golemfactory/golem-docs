a4673547c1ae416cf90dd4c62c6542f2b7539af6
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2023-08-16 15:30:34 +0200

    Fix build output not producing html

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,16 @@ import withSearch from './src/markdoc/search.mjs'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
   experimental: {
     scrollRestoration: true,
   },
   distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default withSearch(
