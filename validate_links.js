da3af504764bd244da06e73d89acd08c83456651
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2023-09-21 10:29:36 +0200

    Validate external links and exclude local/private addresses

--- validate_links.js
+++ validate_links.js
@@ -12,6 +12,30 @@ const foldersToCheck = [
   'src/markdoc/partials/**/*.md',
 ]
 
+const isPrivateOrLocalhost = (url) => {
+  // Regular expression to match private IP addresses and localhost
+  const privateIpPattern =
+    /^(http:\/\/|https:\/\/)?(10\.\d{1,3}\.\d{1,3}\.\d{1,3}|192\.168\.\d{1,3}\.\d{1,3}|172\.(1[6-9]|2[0-9]|3[0-1])\.\d{1,3}\.\d{1,3}|127\.\d{1,3}\.\d{1,3}\.\d{1,3}|localhost)/i
+
+  return privateIpPattern.test(url)
+}
+
+const validateExternalLink = async (href) => {
+  // Added errors as a parameter, assuming it's an array.
+  if (isPrivateOrLocalhost(href)) {
+    return
+  }
+
+  try {
+    const response = await fetch(href, { signal: AbortSignal.timeout(2000) })
+    if (!response.ok) {
+      errors.push(`Broken external link: ${href}`)
+    }
+  } catch {
+    errors.push(`Broken external link: ${href}`)
+  }
+}
+
 foldersToCheck.forEach((folder) => {
   glob(folder, (err, files) => {
     if (err) throw err
@@ -26,27 +50,36 @@ foldersToCheck.forEach((folder) => {
           flag = false
         }
 
-        if (!href.startsWith('http')) {
-          if (!href.startsWith('/')) {
-            errors.push(`Missing / in image link in file ${file}: ${href} \n`)
+        if (href.startsWith('http')) {
+          validateExternalLink(href)
+        } else if (!href.startsWith('/')) {
+          errors.push(`Missing / in image link in file ${file}: ${href} \n`)
+          flag = false
+        } else {
+          const imagePath = path.join(
+            __dirname,
+            'public',
+            href.replace('/', '')
+          )
+          if (!fs.existsSync(imagePath)) {
+            errors.push(`Broken image in file ${file}: linking to ${href} \n`)
             flag = false
-          } else {
-            const imagePath = path.join(
-              __dirname,
-              'public',
-              href.replace('/', '')
-            )
-            if (!fs.existsSync(imagePath)) {
-              errors.push(`Broken image in file ${file}: linking to ${href} \n`)
-              flag = false
-            }
           }
         }
       }
 
-      renderer.link = function (href) {
-        if (!(href.startsWith('http') || href.startsWith('#'))) {
-          const dirname = path.dirname(file)
+      renderer.link = async function (href) {
+        const dirname = path.dirname(file)
+        if (href.startsWith('http')) {
+          if (
+            dirname.startsWith('src/pages/docs/golem-js/reference') ||
+            dirname.startsWith('src/pages/docs/templates')
+          ) {
+            return
+          }
+
+          await validateExternalLink(href)
+        } else if (!href.startsWith('#')) {
           const newHref = path.join(root, `${href.split('#')[0]}.md`)
           const indexHref = path.join(root, `${href.split('#')[0]}/index.md`)
 
@@ -54,7 +87,6 @@ foldersToCheck.forEach((folder) => {
             errors.push(
               `Broken link in file ${file}: linking to ${href} --> Links ending with index are not allowed due to Next.js routing. \n`
             )
-            flag = false
           }
 
           if (href.startsWith('../')) {
@@ -63,19 +95,16 @@ foldersToCheck.forEach((folder) => {
               path.join(dirname, '..', cleanHref.replace('../', '')) + '.md'
             if (!fs.existsSync(newHref)) {
               errors.push(`Broken link in file ${file}: linking to ${href} \n`)
-              flag = false
             }
             return
           }
 
           if (!fs.existsSync(newHref) && !fs.existsSync(indexHref)) {
-            // Skip reference links as it machine generated
             if (dirname.startsWith('src/pages/docs/golem-js/reference')) {
               return
             }
 
             errors.push(`Broken link in file ${file}: linking to ${href} \n`)
-            flag = false
           }
         }
       }
