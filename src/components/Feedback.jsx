4b3ad1a509b0bf4e6f63a58e8a3a475344528675
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2023-09-21 23:04:35 +0200

    Add loading icon to feedback buttons

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -7,9 +7,11 @@ const handleFeedback = (
   feedback,
   setOpen,
   setShowThanks,
-  helpful
+  helpful,
+  setLoading
 ) => {
   if (identifier) {
+    setLoading(true)
     let data = {
       user: localStorage.getItem('GDocsUUID'),
       feedback: feedback === 'yes' ? '' : feedback,
@@ -25,10 +27,12 @@ const handleFeedback = (
       .then((data) => {
         localStorage.setItem(identifier, helpful ? 'yes' : 'no')
         setOpen(false)
+        setLoading(false)
         setShowThanks(true)
       })
       .catch((error) => {
         console.error('Error:', error)
+        setLoading(false)
         setOpen(false)
       })
   } else {
@@ -43,6 +47,7 @@ export function FeedbackButtons({ children, identifier, article = false }) {
   const [isOpen, setOpen] = useState(false)
   const [feedback, setFeedback] = useState('')
   const [showThanks, setShowThanks] = useState(false)
+  const [loading, setLoading] = useState(false)
   const url = article
     ? '/api/feedback/article'
     : '/api/feedback/troubleshooting'
@@ -78,7 +83,9 @@ export function FeedbackButtons({ children, identifier, article = false }) {
 
       <div className="flex justify-center text-xs">
         {showThanks ? (
-          <p className="-my-2 prose pb-4 text-green-500">Thanks for your feedback!</p>
+          <p className="prose -my-2 pb-4 text-green-500">
+            Thanks for your feedback!
+          </p>
         ) : (
           <>
             <button
@@ -89,7 +96,8 @@ export function FeedbackButtons({ children, identifier, article = false }) {
                   feedback,
                   setOpen,
                   setShowThanks,
-                  true
+                  true,
+                  setLoading
                 )
               }
               className={`group-peer group prose mr-4 flex items-center rounded-md border border-primary px-4 py-2 dark:border-darkprimary ${
@@ -101,14 +109,35 @@ export function FeedbackButtons({ children, identifier, article = false }) {
               
               `}
             >
-              <LikeIcon
-                className={`mr-2 h-5 w-5 ${
-                  feedback === 'yes'
-                    ? 'fill-white'
-                    : 'fill-primary group-hover:fill-white dark:fill-darkprimary'
-                }`}
-              />
-              Yes
+              {loading ? (
+                <svg
+                  aria-hidden="true"
+                  class="mr-2 h-5 w-5 animate-spin fill-primary text-gray-200 dark:text-gray-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+              ) : (
+                <>
+                  <LikeIcon
+                    className={`mr-2 h-5 w-5 ${
+                      feedback === 'yes'
+                        ? 'fill-white'
+                        : 'fill-primary group-hover:fill-white dark:fill-darkprimary'
+                    }`}
+                  />
+                  Yes
+                </>
+              )}
             </button>
             <button
               onClick={() => setOpen(true)}
@@ -118,14 +147,35 @@ export function FeedbackButtons({ children, identifier, article = false }) {
                   : 'text-primary hover:bg-primaryhover hover:text-white dark:text-white dark:hover:bg-darkprimary'
               }`}
             >
-              <DislikeIcon
-                className={`mr-2 h-5 w-5 ${
-                  feedback === 'no'
-                    ? 'fill-white'
-                    : 'fill-primary group-hover:fill-white dark:fill-darkprimary'
-                }`}
-              />
-              No
+              {loading ? (
+                <svg
+                  aria-hidden="true"
+                  class=" h-5 w-5 animate-spin fill-primary text-gray-200 dark:text-gray-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+              ) : (
+                <>
+                  <DislikeIcon
+                    className={`mr-2 h-5 w-5 ${
+                      feedback === 'no'
+                        ? 'fill-white'
+                        : 'fill-primary group-hover:fill-white dark:fill-darkprimary'
+                    }`}
+                  />
+                  No
+                </>
+              )}
             </button>
           </>
         )}
@@ -137,6 +187,7 @@ export function FeedbackButtons({ children, identifier, article = false }) {
         setOpen={setOpen}
         showThanks={showThanks}
         setShowThanks={setShowThanks}
+        setLoading={setLoading}
       />
     </div>
   )
@@ -151,6 +202,7 @@ export const FeedbackModal = ({
   setOpen,
   showThanks,
   setShowThanks,
+  setLoading,
 }) => {
   const cancelButtonRef = useRef(null)
   const [feedback, setFeedback] = useState('')
@@ -223,7 +275,8 @@ export const FeedbackModal = ({
                         feedback,
                         setOpen,
                         setShowThanks,
-                        false
+                        false,
+                        setLoading
                       )
                     }
                   >
