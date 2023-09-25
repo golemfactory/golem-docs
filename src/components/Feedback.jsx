30138e17d5804064952faa88ae63569a99e6f83a
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2023-09-25 14:14:19 +0200

    Feature flag Feedback buttons via env variable

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -63,134 +63,138 @@ export function FeedbackButtons({ children, identifier, article = false }) {
     if (localStorageFeedback) setFeedback(localStorageFeedback)
   }, [])
 
-  return (
-    <div
-      className={`
+  if (process.env.NEXT_PUBLIC_SHOW_FEEDBACK_BUTTONS === 'true') {
+    return (
+      <div
+        className={`
     ${
       article
         ? 'my-6 w-full rounded-md bg-lightblue py-12 text-center dark:bg-darkcontent'
         : '-mt-4'
     }
     `}
-    >
-      <p
-        className={`
+      >
+        <p
+          className={`
         mb-4
         mr-4 text-center font-semibold text-black dark:text-white`}
-      >
-        Was this helpful?
-      </p>
+        >
+          Was this helpful?
+        </p>
 
-      <div className="flex justify-center text-xs">
-        {showThanks ? (
-          <p className="prose -my-2 pb-4 text-green-500">
-            Thanks for your feedback!
-          </p>
-        ) : (
-          <>
-            <button
-              onClick={() =>
-                handleFeedback(
-                  url,
-                  identifier,
-                  feedback,
-                  setOpen,
-                  setShowThanks,
-                  true,
-                  setLoading
-                )
-              }
-              className={`group-peer group prose mr-4 flex items-center rounded-md border border-primary px-4 py-2 dark:border-darkprimary ${
-                feedback === 'yes'
-                  ? 'bg-primaryhover text-white dark:bg-darkprimary'
-                  : 'text-primary hover:bg-primaryhover hover:text-white dark:text-white dark:hover:bg-darkprimary'
-              }
+        <div className="flex justify-center text-xs">
+          {showThanks ? (
+            <p className="prose -my-2 pb-4 text-green-500">
+              Thanks for your feedback!
+            </p>
+          ) : (
+            <>
+              <button
+                onClick={() =>
+                  handleFeedback(
+                    url,
+                    identifier,
+                    feedback,
+                    setOpen,
+                    setShowThanks,
+                    true,
+                    setLoading
+                  )
+                }
+                className={`group-peer group prose mr-4 flex items-center rounded-md border border-primary px-4 py-2 dark:border-darkprimary ${
+                  feedback === 'yes'
+                    ? 'bg-primaryhover text-white dark:bg-darkprimary'
+                    : 'text-primary hover:bg-primaryhover hover:text-white dark:text-white dark:hover:bg-darkprimary'
+                }
               
               
               `}
-            >
-              {loading ? (
-                <svg
-                  aria-hidden="true"
-                  class="mr-2 h-5 w-5 animate-spin fill-primary text-gray-200 dark:fill-white dark:text-gray-600"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="currentColor"
-                  />
-                  <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentFill"
-                  />
-                </svg>
-              ) : (
-                <>
-                  <LikeIcon
-                    className={`mr-2 h-5 w-5 ${
-                      feedback === 'yes'
-                        ? 'fill-white'
-                        : 'fill-primary group-hover:fill-white dark:fill-darkprimary'
-                    }`}
-                  />
-                  Yes
-                </>
-              )}
-            </button>
-            <button
-              onClick={() => setOpen(true)}
-              className={`group-peer group prose mr-4 flex items-center rounded-md border border-primary px-4 py-2 dark:border-darkprimary ${
-                feedback === 'no'
-                  ? 'bg-primaryhover text-white dark:bg-darkprimary'
-                  : 'text-primary hover:bg-primaryhover hover:text-white dark:text-white dark:hover:bg-darkprimary'
-              }`}
-            >
-              {loading ? (
-                <svg
-                  aria-hidden="true"
-                  class=" h-5 w-5 animate-spin fill-primary text-gray-200 dark:fill-white dark:text-gray-600"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="currentColor"
-                  />
-                  <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentFill"
-                  />
-                </svg>
-              ) : (
-                <>
-                  <DislikeIcon
-                    className={`mr-2 h-5 w-5 ${
-                      feedback === 'no'
-                        ? 'fill-white'
-                        : 'fill-primary group-hover:fill-white dark:fill-darkprimary'
-                    }`}
-                  />
-                  No
-                </>
-              )}
-            </button>
-          </>
-        )}
+              >
+                {loading ? (
+                  <svg
+                    aria-hidden="true"
+                    class="mr-2 h-5 w-5 animate-spin fill-primary text-gray-200 dark:fill-white dark:text-gray-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                ) : (
+                  <>
+                    <LikeIcon
+                      className={`mr-2 h-5 w-5 ${
+                        feedback === 'yes'
+                          ? 'fill-white'
+                          : 'fill-primary group-hover:fill-white dark:fill-darkprimary'
+                      }`}
+                    />
+                    Yes
+                  </>
+                )}
+              </button>
+              <button
+                onClick={() => setOpen(true)}
+                className={`group-peer group prose mr-4 flex items-center rounded-md border border-primary px-4 py-2 dark:border-darkprimary ${
+                  feedback === 'no'
+                    ? 'bg-primaryhover text-white dark:bg-darkprimary'
+                    : 'text-primary hover:bg-primaryhover hover:text-white dark:text-white dark:hover:bg-darkprimary'
+                }`}
+              >
+                {loading ? (
+                  <svg
+                    aria-hidden="true"
+                    class=" h-5 w-5 animate-spin fill-primary text-gray-200 dark:fill-white dark:text-gray-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                ) : (
+                  <>
+                    <DislikeIcon
+                      className={`mr-2 h-5 w-5 ${
+                        feedback === 'no'
+                          ? 'fill-white'
+                          : 'fill-primary group-hover:fill-white dark:fill-darkprimary'
+                      }`}
+                    />
+                    No
+                  </>
+                )}
+              </button>
+            </>
+          )}
+        </div>
+        <FeedbackModal
+          url={url}
+          identifier={identifier}
+          open={isOpen}
+          setOpen={setOpen}
+          showThanks={showThanks}
+          setShowThanks={setShowThanks}
+          setLoading={setLoading}
+        />
       </div>
-      <FeedbackModal
-        url={url}
-        identifier={identifier}
-        open={isOpen}
-        setOpen={setOpen}
-        showThanks={showThanks}
-        setShowThanks={setShowThanks}
-        setLoading={setLoading}
-      />
-    </div>
-  )
+    )
+  } else {
+    return null
+  }
 }
 
 import { useRef } from 'react'
