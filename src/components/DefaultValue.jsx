c302a62f95aea3b76f32ffeb34fae9972bb3e8cc
Phillip Jensen <phillip@golemgrid.com>
2023-10-26 14:27:26 +0200

    Fix sidebar bug

--- src/components/DefaultValue.jsx
+++ src/components/DefaultValue.jsx
@@ -1,11 +1,11 @@
 import { slugifyWithCounter } from '@sindresorhus/slugify'
 const slugify = slugifyWithCounter()
 
-function DefaultValue({ title, description, defaultValue }) {
+function DefaultValue({ title, description, defaultValue, id }) {
     return (
         <div className="border border-lightbluedarker dark:border-none bg-white dark:bg-darkcontent p-6 rounded-lg mb-6 shadow-sm transition-shadow duration-200">
             <h3
-                id={slugify(title)}
+                id={id}
              className="font-semibold text-xl mb-4 text-primary dark:text-darkprimary">{title}</h3>
             <p className="text-gray-700 dark:text-white dark:text-opacity-50 mb-4">{description}</p>
             <div className="mt-2">
