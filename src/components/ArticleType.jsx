5f408d714b2a7cc9584041c300d2512e6743278b
Phillip Jensen <phillip@golemgrid.com>
2023-11-06 09:50:53 +0100

    Fix guide icon being fully white with white background

--- src/components/ArticleType.jsx
+++ src/components/ArticleType.jsx
@@ -10,7 +10,7 @@ export function ArticleType({ type, children }) {
   return (
     <div className="inline-flex items-center gap-x-2 rounded-2xl bg-lightbluedarker px-1.5 py-1 font-medium dark:bg-darkcontent dark:text-white">
       {type.toLowerCase() === 'guide' ? (
-        <TutorialIcon className="h-6 w-6 text-white dark:fill-white" />
+        <TutorialIcon className="h-6 w-6 text-white " />
       ) : type.toLowerCase() === 'tutorial' ? (
         <ExampleIcon className="h-6 w-6 text-white dark:fill-white" />
       ) : type.toLowerCase() === 'example' ? (
